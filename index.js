a2badce15b757a3a69cf29c62cc31c552296f1c2
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-06-10 17:14:15 +0600

    token for cokkie added\

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        // 'https://volunteer-91281.web.app',
+        // 'https://volunteer-91281.firebaseapp.com'
+    ],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gnbvncz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -24,6 +34,34 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+    console.log("log info - ", req.method, req.url)
+    next()
+}
+
+const verifyToken = (req, res, next) => {
+    // console.log(req.cookies)
+    const token = req?.cookies?.token;
+    console.log('token in the middleware -', token);
+    // no token available 
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access - no token' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+const cookieOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +71,23 @@ async function run() {
         const paymentCollection = client.db('ProbizDB').collection('payment')
         const workCollection = client.db('ProbizDB').collection('works')
         const messageCollection = client.db('ProbizDB').collection('message')
+
+        app.post('/jwt',logger, async (req, res) => {
+            const user = req.body
+            console.log('user for token', user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.cookie('token', token, cookieOption).send({ success: true })
+        })
+        
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            console.log('logging out', user)
+            res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ success: true })
+        })
+
 
         app.post('/users', async (req, res) => {
             const user = req.body
